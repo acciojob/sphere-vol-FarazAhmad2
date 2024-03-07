4cffe31581473d7c24874d312bd1712cbb24a8d4
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-07 14:49:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function volume_sphere() {
     //Write your code here
+	// Get the value of radius input
+    let radiusValue = parseFloat(document.getElementById("radius").value);
+    if (radiusValue <0) {
+        document.getElementById("volume").value = NaN;
+    } else {
+
+    // Calculate the volume of the sphere
+    let volume = (4/3) * Math.PI * Math.pow(radiusValue, 3);
+
+    // Set the calculated volume to the volume input field
+    document.getElementById("volume").value = volume.toFixed(4);
+    }
   
 } 
 
